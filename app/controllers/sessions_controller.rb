e3359dea1077b81b15e3e89eb7de782edc67e425
sitz_bnk21 <146817192+IbukiShinzato@users.noreply.github.com>
2023-10-31 09:03:41 +0000

    finish ch9_1_2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      remember user
       reset_session
       log_in user
       redirect_to user
