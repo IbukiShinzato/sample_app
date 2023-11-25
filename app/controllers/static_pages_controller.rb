986ae2e805a747b737b618c345bc1032e8c4cdf6
Ibuki Shinzato <e235718@ie.u-ryukyu.ac.jp>
2023-11-25 05:33:02 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,19 @@
 class StaticPagesController < ApplicationController
+
   def home
-    # render ....
-    # => app/views/static_pages/home.html.erb
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
-  # app/views/コントローラ名/アクション名.html.erb
   def help
-    # => app/views/static_pages/help.html.erb
-    # roots / root / rooting
-    # routes / route / routing
   end
 
-  def about 
-    # => app/views/static_pages/about.html.erb
+  def about
   end
 
   def contact
-    # => app/views/static_pages/contact.html.erb
   end
 end
+
